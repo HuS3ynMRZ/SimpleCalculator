ac141d5dab73cf6a3260a97cc3d92d5e83bfe807
Huseyn <90988414+HuS3ynMRZ@users.noreply.github.com>
2022-12-26 11:48:22 +0200

    www

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     EditText number1;
     EditText number2;
-    TextView result;
+    TextView resultText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,19 +18,56 @@ public class MainActivity extends AppCompatActivity {
 
         number1 = findViewById(R.id.number1);
         number2 = findViewById(R.id.number2);
-        result = findViewById(R.id.textView);
+        resultText = findViewById(R.id.resultText);
     }
 
     public void sum(View view){
-        
+        if(number1.getText().toString().matches("") || number2.getText().toString().matches("")){
+            resultText.setText("No number");
+        } else {
+            double n1 = Double.parseDouble(number1.getText().toString());
+            double n2 = Double.parseDouble(number2.getText().toString());
+
+            double result = n1 + n2;
+
+            resultText.setText("Result: " + result);
+        }
     }
     public void divide(View view){
+        if(number1.getText().toString().matches("") || number2.getText().toString().matches("")){
+            resultText.setText("No number");
+        } else {
+            double n1 = Double.parseDouble(number1.getText().toString());
+            double n2 = Double.parseDouble(number2.getText().toString());
 
+            double result = n1 / n2;
+
+            resultText.setText("Result: " + result);
+        }
     }
     public void multiply(View view){
+        if(number1.getText().toString().matches("") || number2.getText().toString().matches("")){
+            resultText.setText("No number");
+        } else {
+            double n1 = Double.parseDouble(number1.getText().toString());
+            double n2 = Double.parseDouble(number2.getText().toString());
 
+            double result = n1 * n2;
+
+            resultText.setText("Result: " + result);
+        }
     }
     public void substract(View view){
+        if(number1.getText().toString().matches("") || number2.getText().toString().matches("")){
+            resultText.setText("No number");
+        } else {
+            double n1 = Double.parseDouble(number1.getText().toString());
+            double n2 = Double.parseDouble(number2.getText().toString());
 
+            double result = n1 - n2;
+
+            resultText.setText("Result: " + result);
+        }
     }
+
 }
